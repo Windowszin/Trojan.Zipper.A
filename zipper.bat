86278efe9ec2b3e323701b002317dc1603603290
Windowszin <125829699+Windowszin@users.noreply.github.com>
2023-07-15 14:16:07 -0300

    Add files via upload

---
+++ zipper.bat
@@ -0,0 +1,13 @@
+@echo off
+
+set "Desktop=%USERPROFILE%\Desktop"
+
+set "VictimFolder=%USERPROFILE%\Downloads"
+
+
+echo (7zip) Zipping files...
+
+7z a "%Desktop%\%USERNAME%Files.zip" "%VictimFolder%\*"
+
+echo Zip file created successfully!
+
